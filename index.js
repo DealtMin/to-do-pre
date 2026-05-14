e5838f51b0251bab4152742e4834c09f6791017f
DealtMin <dealtmin@gmail.com>
2026-05-15 02:20:18 +0300

    fix3???

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let items = [
-	"Сделать проектную работу",
-	"Полить цветы",
-	"Пройти туториал по Реакту",
-	"Сделать фронт для своего проекта",
-	"Прогуляться по улице в солнечный день",
-	"Помыть посуду",
+    "Сделать проектную работу",
+    "Полить цветы",
+    "Пройти туториал по Реакту",
+    "Сделать фронт для своего проекта",
+    "Прогуляться по улице в солнечный день",
+    "Помыть посуду",
 ];
 
 const listElement = document.querySelector(".to-do__list");
@@ -37,13 +37,14 @@ function loadTasks() {
 }
 
 function createItem(item) {
-	const template = document.getElementById("to-do__item-template");
-	const clone = template.content.querySelector(".to-do__item").cloneNode(true);
-	const textElement = clone.querySelector(".to-do__item-text");
-	const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
-	const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
-	const editButton = clone.querySelector(".to-do__item-button_type_edit");
-	editButton.addEventListener('click', function(event) {
+    const template = document.getElementById("to-do__item-template");
+    const clone = template.content.querySelector(".to-do__item").cloneNode(true);
+    const textElement = clone.querySelector(".to-do__item-text");
+    const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
+    const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
+    const editButton = clone.querySelector(".to-do__item-button_type_edit");
+
+    editButton.addEventListener('click', function(event) {
         event.preventDefault();
         const index = items.indexOf(textElement.textContent);
         textElement.contentEditable = 'true';
@@ -88,4 +89,3 @@ function saveTasks(tasks) {
     if (tasks.length === 0)
         localStorage.removeItem('tasks');
 }
-
